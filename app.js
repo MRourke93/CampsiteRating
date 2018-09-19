3b1bfdb8401dd8fe76d20418574c3c78e420ac30
Matthew Rourke <mrourke93@gmail.com>
2018-09-19 01:31:03 +0000

    add mLab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes       = require("./routes/comments"),
     authRoutes          = require("./routes/index");
     
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://matt:password1@ds161322.mlab.com:61322/mattyelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
